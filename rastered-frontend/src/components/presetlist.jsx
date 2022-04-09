223aca689b2425c4597a04c8ed508729bd3b71ed
Pascal Matthäus <pascalmatthaeus@live.de>
2022-04-09 22:11:05 +0200

    React.js: Progress on Pipeline creation view.

--- rastered-frontend/src/components/presetlist.jsx
+++ rastered-frontend/src/components/presetlist.jsx
@@ -18,6 +18,8 @@ const PipelineZone = styled.div`
 	display: flex;
 	flex-direction: column;
 	flex: auto;
+	background-color: #111111;
+	border-radius: 8px;
 `;
 
 const ZoneTitle = styled.p`
@@ -29,12 +31,12 @@ const AvailablePreset = styled.div`
 	/*border: 1px solid red;*/
 	text-align: center;
 	user-select: none;
-	flex: 0 0 18%;
+	flex: 0 0 19%;
 	background-color: #061223;
 	box-shadow: 0px 3px 10px rgba(0,0,0,0.3);
 	border-radius: 18px;
 	padding: 1%;
-	margin: 1%;
+	margin: 0.5%;
 	&:hover {
 		background-color: #21a7eb;
 	}
@@ -55,15 +57,37 @@ const SelectedPreset = styled.div`
 	}
 `;
 
+const SelectedPresetDraggable = (props) => {
+	const handleDrag = e => {
+		console.log(e.pageX);
+	};
+	
+	const handleDragOver = e => {
+		console.log(e.nativeEvent.target);
+	};
+	
+	return (
+		<SelectedPreset 
+		draggable droppable
+		onDrag={handleDrag}
+		onDragOver={handleDragOver}>
+		
+			{props.name}
+		
+		</SelectedPreset>
+	);
+};
+
 export const PresetList = (props) => 
 {
-	const [presets,setPresets] = useState([]);
+	const [presets,setPresets] = useState([{}]);
 
-	useEffect ( () => {
-		const response = fetch("https://app.rastered.io/getpresets")
-			.then( response => response.json() )
-			.then( setPresets )
-	},[]);
+	useEffect ( async () => {
+		const response = await fetch("https://app.rastered.io/getpresets");
+		const json = await response.json();
+		await setPresets( json );
+		},[]);
+	
 	
 	console.log(presets);
 	
@@ -71,21 +95,21 @@ export const PresetList = (props) =>
 		<Container>
 		<ZoneTitle>AVAILABLE PRESETS</ZoneTitle>
 		<AvailablePresetsZone>
-			{ presets.map( (preset) => 
+			{ presets.map( preset => 
 				<AvailablePreset>{preset.friendlyName}</AvailablePreset>
 			) }
-			<AvailablePreset>Bloom</AvailablePreset>
-			<AvailablePreset>Gaussian Blur</AvailablePreset>
-			<AvailablePreset>Cartoonize</AvailablePreset>
-			<AvailablePreset>FFT</AvailablePreset>
-			<AvailablePreset>Sketch</AvailablePreset>
-			<AvailablePreset>Unsharp Mask</AvailablePreset>
+			{/*<AvailablePresetDraggable name="Bloom"/>
+			<AvailablePresetDraggable name="Gaussian Blur"/>
+			<AvailablePresetDraggable name="Cartoonize"/>
+			<AvailablePresetDraggable name="FFT"/>
+			<AvailablePresetDraggable name="Sketch"/>
+			<AvailablePresetDraggable name="Unsharp Mask"/>*/}
 		</AvailablePresetsZone>
 		<p/>
 		<ZoneTitle>PIPELINE</ZoneTitle>
 		<PipelineZone>
-			{ presets.map( (preset) => 
-			<SelectedPreset>{preset.friendlyName}</SelectedPreset>
+			{ presets.map( preset => 
+			<SelectedPresetDraggable name={preset.friendlyName} />
 			) }
 		</PipelineZone>
 		</Container>
